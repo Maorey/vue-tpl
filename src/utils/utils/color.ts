58c33c5184282f5301391a1e78b6bd54f068c6e9
毛瑞 <Maorey@Foxmail.com>
2019-07-04 14:38:28 +0800

    Add: 颜色处理

--- src/utils/utils/color.ts
+++ src/utils/utils/color.ts
@@ -3,27 +3,32 @@
  * @Author: 毛瑞
  * @Date: 2019-07-03 14:48:49
  * @LastEditors: 毛瑞
- * @LastEditTime: 2019-07-04 12:37:44
+ * @LastEditTime: 2019-07-04 14:34:06
  */
 
 /** hex3/hex颜色
  */
-const REG_HEX = /#([0-9a-f]{3,8})/i //
-/** rgb/rgba颜色
+const REG_HEX = /#([0-9a-f]{3,8})/i
+/** rgb/rgba颜色 先去空格?
  */
-const REG_RGB = /rgba? *\( *(\d+ *, *\d+ *, *\d+) *,? *(\d+\.?\d+?)? *\)/i
+const REG_RGB = /rgba?\s*\(\s*(\d+\s*,\s*\d+\s*,\s*\d+)\s*,?\s*(\d+\.?\d+?)?\s*\)/i
+/** 转为10进制整数
+ * parseInt(string, radix)
+ * Number.prototype.toString(radix) 数字toString可以设置进制,默认10
+ */
+const PARSEINT = (str: string): number => parseInt(str)
 /** 颜色转rgb(a)
  * @param {String} color rgb\rgba\hex\hex3 颜色
  * @param {Number|Function} opacity 要设置的透明度，若回调则接受当前值返回新值
  * [0 - 1] 小于0视为0 大于等于1返回rgb 其它保留原透明度
- * @param {Function} filter 自定义颜色处理方法 接受参数为rgb色值数组，应返回或直接修改该数组
+ * @param {Function} filter 自定义颜色处理方法 直接修改rgb数组 返回透明度
  *
- * @returns {String}
+ * @returns {String} rgb(a) 颜色
  */
-function toGRB(
+function toRGB(
   color: string,
-  opacity: number | ((alpha: number) => number | any) = 1,
-  filter?: (rgba: number[]) => number[] | void
+  opacity?: number | ((alpha: number) => number | any) | null,
+  filter?: (rgb: number[], alpha: number) => number | any
 ): string {
   color = color.trim()
   // 非字符串或空字符串返回空字符串
@@ -60,7 +65,7 @@ function toGRB(
     tmp = REG_RGB.exec(color)
     if (tmp) {
       // 色值整数
-      rgb = tmp[1].split(',').map((str: string) => parseInt(str))
+      rgb = tmp[1].split(',').map(PARSEINT)
       alpha = parseFloat(tmp[2]) // 无透明度为NaN
     } else {
       // 不支持的字符串原样返回
@@ -69,22 +74,17 @@ function toGRB(
   }
 
   // 透明度处理
-  if (opacity !== undefined) {
-    if (typeof opacity === 'function') {
-      alpha = opacity(isNaN(alpha) ? 1 : alpha)
-    } else {
-      alpha = opacity
-    }
+  if (typeof opacity === 'number') {
+    alpha = opacity
+  } else if (opacity) {
+    alpha = opacity(isNaN(alpha) ? 1 : alpha)
   }
   alpha = isNaN(alpha) ? 1 : Math.max(0, alpha) // 使大于等于0
 
   // 自定义颜色处理
   if (filter) {
-    tmp = filter([...rgb, alpha])
-    if (tmp) {
-      alpha = tmp.pop()
-      rgb = tmp
-    }
+    tmp = filter(rgb, alpha)
+    isNaN(tmp) || (alpha = tmp)
   }
 
   if (alpha < 1) {
@@ -94,4 +94,71 @@ function toGRB(
   return 'rgb(' + rgb.toString() + ')'
 }
 
-export { toGRB }
+/** 颜色是否透明
+ * @param {String} color rgb\rgba\hex\hex3 颜色
+ *
+ * @returns {Boolean}
+ */
+function isTransparent(color: string): boolean {
+  let transparent = true
+
+  toRGB(color, (alpha: number) => {
+    transparent = alpha <= 0
+  })
+
+  return transparent
+}
+
+/** 自适应颜色
+ * @param {String} color rgb\rgba\hex\hex3 颜色
+ * @param {Number} ratio [0, 1] 越接近1与原颜色越相似
+ * @param {Number|Function} opacity 要设置的透明度，若回调则接受当前值返回新值
+ * [0 - 1] 小于0视为0 大于等于1返回rgb 其它保留原透明度
+ *
+ * @returns {String} rgb(a) 颜色
+ */
+function fitColor(
+  color: string,
+  ratio: number = 0.25,
+  opacity?: number | ((alpha: number) => number | any) | null
+): string {
+  return toRGB(
+    color,
+    opacity,
+    (rgb: number[]): void => {
+      let value: number
+      let i: number = 3
+      while (i--) {
+        value = rgb[i]
+        rgb[i] =
+          value +
+          (value > 88
+            ? -Math.floor(value * ratio) // 加深
+            : Math.floor((255 - value) * ratio)) // 变浅
+      }
+    }
+  )
+}
+
+const FILLER_REVERSE = (rgb: number[]): void => {
+  let i: number = 3
+  while (i--) {
+    rgb[i] = 255 - rgb[i]
+  }
+}
+/** 计算反色
+ * @param {String} color rgb\rgba\hex\hex3 颜色
+ * @param {Function} filter 自定义颜色处理方法 直接修改rgb数组 返回透明度
+ * @param {Number|Function} opacity 要设置的透明度[0 - 1] 小于0视为0 大于等于1返回rgb 其它保留原透明度
+ *
+ * @returns {String} rgb(a) 颜色
+ */
+function reverseColor(
+  color: string,
+  filter: (rgb: number[], alpha: number) => number | any = FILLER_REVERSE,
+  opacity?: number | ((alpha: number) => number | any) | null
+): string {
+  return toRGB(color, opacity, filter)
+}
+
+export { toRGB, isTransparent, fitColor, reverseColor }
--- tests/unit/utils/utils/color.spec.ts
+++ tests/unit/utils/utils/color.spec.ts
@@ -3,17 +3,52 @@
  * @Author: 毛瑞
  * @Date: 2019-07-03 17:26:45
  * @LastEditors: 毛瑞
- * @LastEditTime: 2019-07-03 17:32:28
+ * @LastEditTime: 2019-07-04 14:37:08
  */
 
-import { toGRB } from '@/utils/utils/color'
+import { toRGB, isTransparent, reverseColor } from '@/utils/utils/color'
 
 const HEX1 = '#abc'
-const HEX2 = '#abcd'
 const RGB1 = 'rgb(170,187,204)'
-const RGB2 = ''
+const RGB1_REVERSE = 'rgb(85,68,51)'
+
+const HEX2 = '#aabbccdd'
+const RGB2 = 'rgba(170,187,204,0.86)'
+const RGB2_REVERSE = 'rgba(85,68,51,0.86)'
+
+const REVERSE_FILLER = (rgb: number[]): void => {
+  let i: number = 3
+  while (i--) {
+    rgb[i] = 255 - rgb[i]
+  }
+}
 
 test('color to rgb(a)', () => {
-  expect(toGRB(HEX1)).toBe(RGB1)
-  expect(toGRB(HEX2)).toBe(RGB2)
+  expect(toRGB(HEX1)).toBe(RGB1)
+  expect(toRGB(HEX2)).toBe(RGB2)
+  expect(toRGB(' rgba (  1 , 2 , 3 , 0.5 ) ')).toBe('rgba(1,2,3,0.50)')
+
+  const RGBA1 = 'rgba(170,187,204,0.50)'
+  expect(toRGB(HEX1, 0.5)).toBe(RGBA1)
+  expect(toRGB(HEX1, () => 0.5)).toBe(RGBA1)
+
+  expect(toRGB(HEX1, null, REVERSE_FILLER)).toBe(RGB1_REVERSE)
+  expect(toRGB(HEX1, () => 0.5, REVERSE_FILLER)).toBe('rgba(85,68,51,0.50)')
+})
+
+test('isTransparent', () => {
+  expect(isTransparent(HEX1)).toBe(false)
+  expect(isTransparent(HEX2)).toBe(false)
+  expect(isTransparent(RGB1_REVERSE)).toBe(false)
+  expect(isTransparent(RGB2_REVERSE)).toBe(false)
+
+  expect(isTransparent('transparent')).toBe(true)
+  expect(isTransparent('#9870')).toBe(true)
+  expect(isTransparent('#abcdef00')).toBe(true)
+  expect(isTransparent('rgba(12,34,56,0)')).toBe(true)
+})
+
+test('reverseColor', () => {
+  expect(reverseColor(HEX1)).toBe(RGB1_REVERSE)
+  expect(reverseColor(HEX2)).toBe(RGB2_REVERSE)
 })
