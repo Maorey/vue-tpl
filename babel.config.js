5e5200bb28e85bec013b716609e2de38a691fa8e
毛瑞 <Maorey@Foxmail.com>
2019-07-04 17:34:57 +0800

    Up: 更新包

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@
  * @Author: 毛瑞
  * @Date: 2019-06-18 15:58:46
  * @LastEditors: 毛瑞
- * @LastEditTime: 2019-07-02 09:42:45
+ * @LastEditTime: 2019-07-04 16:49:55
  */
 module.exports = {
   presets: ['@vue/app'], // https://cli.vuejs.org/zh/config/#babel
@@ -13,12 +13,13 @@ module.exports = {
     // ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
     // '@babel/plugin-proposal-class-properties',
     // polyfill 工具 https://babeljs.io/docs/en/next/babel-plugin-transform-runtime.html
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        corejs: 2, // 用3.x vue 部分不兼容...
-        helpers: false,
-      },
-    ],
+    // @babel/runtime-corejs3
+    // [
+    //   '@babel/plugin-transform-runtime',
+    //   {
+    //     corejs: 3,
+    //     helpers: false,
+    //   },
+    // ],
   ],
 }
