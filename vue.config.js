b3fc1000cf635619f91321bb1113f391573cf0e6
毛瑞 <Maorey@Foxmail.com>
2019-07-23 13:25:24 +0800

    Fix: 单元测试啥的

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@
  * @Author: 毛瑞
  * @Date: 2019-06-18 16:18:18
  * @LastEditors: 毛瑞
- * @LastEditTime: 2019-07-22 11:02:10
+ * @LastEditTime: 2019-07-23 12:57:58
  */
 // TODO: 环境变量/入口文件 改变热更新
 const path = require('path')
@@ -64,7 +64,8 @@ module.exports = {
         // https://github.com/webpack-contrib/css-loader#localidentname
         // https://github.com/webpack/loader-utils#interpolatename
         localIdentName: isProd ? '[hash:5]' : '[folder]-[name]-[local][emoji]',
-        camelCase: 'only', // 驼峰化class名，不保留非驼峰class名
+        camelCase: 'only', // 只允许驼峰class名
+        // localsConvention: 'camelCaseOnly', // 只允许驼峰class名
       },
       sass: {
         data: '@import "@/scss/var.scss";', // 全局引入
