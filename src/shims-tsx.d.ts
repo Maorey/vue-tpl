60d3948a53024f57b768335739fe049f1bd0fdab
毛瑞 <Maorey@Foxmail.com>
2019-07-24 17:32:46 +0800

    Mod: tsx支持

--- src/shims-tsx.d.ts
+++ src/shims-tsx.d.ts
@@ -6,3 +6,8 @@
 
 // see: https://github.com/wonderful-panda/vue-tsx-support
 import 'vue-tsx-support/enable-check'
+import 'vue-tsx-support/options/allow-element-unknown-attrs'
+import 'vue-tsx-support/options/allow-unknown-props'
+import 'vue-tsx-support/options/enable-vue-router'
+import 'vue-tsx-support/options/enable-html-attrs'
+import 'vue-tsx-support/options/enable-nativeon'
